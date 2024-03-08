efd1a508f10798af7b7cb54ffb115633c07c5827
Niko Sobocan <Niko.Sobocan@comtradegaming.com>
2024-03-08 14:47:17 +0100

    Added tests.

---
+++ MetaExchange/Application/Services/OrderService/OrderService.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,160 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Moq;
+using OrderManagement.Interfaces;
+using OrderManagement.Interfaces.Enums;
+using OrderService.Impl.Services;
+using OrderService.Interfaces.Services;
+using System.Configuration;
+
+namespace OrderService.Tests.Services
+{
+    [TestClass]
+  public class OrderServiceTests
+  {
+    private IFixture? _fixture;
+
+    private IOrderService _orderService = null!;
+    private Mock<IOrderManager> _orderManagerMock = null!;
+
+    [TestInitialize]
+    public void InitTest()
+    {
+      _fixture = new Fixture().Customize(new AutoMoqCustomization { ConfigureMembers = true });
+      _orderManagerMock = _fixture.Freeze<Mock<IOrderManager>>();
+      _orderService = _fixture.Create<Impl.Services.OrderService>();
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_Should_ReturnOrderExecution()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Buy, 1.5M);
+
+      _orderManagerMock.Verify(x => x.GetOrderBooks(It.IsAny<int>()), Times.Once);
+      orderExecution.Count.Should().Be(2);
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_InsufficientExchangeBTCBalance_Should_ThrowException()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      await Assert.ThrowsExceptionAsync<Exception>(async () =>
+      {
+        IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Buy, 1000);
+      });
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_InsufficientExchangeEURBalance_Should_ThrowException()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      await Assert.ThrowsExceptionAsync<Exception>(async () =>
+      {
+        IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Sell, 1000);
+      });
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_BuyOrder_Should_AdjustExchangeBalance()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      List<OrderBook> orderBooksCopy = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Buy, 1);
+
+      orderBooks[0].BalanceBTC.Should().BeLessThan(orderBooksCopy[0].BalanceBTC);
+      orderBooks[0].BalanceEUR.Should().BeGreaterThan(orderBooksCopy[0].BalanceEUR);
+
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_SellOrder_Should_AdjustExchangeBalance()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      List<OrderBook> orderBooksCopy = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Sell, 1);
+
+      orderBooks[0].BalanceBTC.Should().BeGreaterThan(orderBooksCopy[0].BalanceBTC);
+      orderBooks[0].BalanceEUR.Should().BeLessThan(orderBooksCopy[0].BalanceEUR);
+    }
+
+    [TestMethod]
+    public async Task GetOptimalOrderExecution_NotEnoughOrdersInBook_Should_ThrowException()
+    {
+      List<OrderBook> orderBooks = GetMockOrderBookData();
+
+      _orderManagerMock.Setup(x => x.GetOrderBooks(It.IsAny<int>())).ReturnsAsync(orderBooks);
+
+      await Assert.ThrowsExceptionAsync<Exception>(async () =>
+      {
+        IList<Order> orderExecution = await _orderService.GetOptimalOrderExecution(OrderTypeEnum.Buy, 11);
+      });
+
+    }
+
+    private List<OrderBook> GetMockOrderBookData()
+    {
+      return new List<OrderBook> {
+        new OrderBook {
+          Id = 1,
+          BalanceBTC = 100,
+          BalanceEUR = 100000,
+          Asks = new List <OrderWrapper> {
+            new OrderWrapper {
+              Order = new Order {
+                OrderBookId = 1,
+                Amount = 1,
+                Price = 1000,
+                Type = OrderTypeEnum.Sell
+              }
+            },
+            new OrderWrapper {
+              Order = new Order {
+                OrderBookId = 1,
+                Amount = 2,
+                Price = 1100,
+                Type = OrderTypeEnum.Sell
+              }
+            }
+          },
+          Bids = new List <OrderWrapper> {
+            new OrderWrapper {
+                Order = new Order {
+                OrderBookId = 1,
+                Amount = 1,
+                Price = 1500,
+                Type = OrderTypeEnum.Buy
+              }
+            },
+            new OrderWrapper {
+                Order = new Order {
+                OrderBookId = 1,
+                Amount = 2,
+                Price = 1600,
+                Type = OrderTypeEnum.Buy
+              }
+            }
+          }
+        },
+      };
+    }
+  }
+}
